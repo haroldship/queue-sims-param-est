b9c85e1285e0c8ea871ba52b82867d21890290a1
Harold Ship <harold@il.ibm.com>
2023-01-08 20:24:11 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x01, x02, x03 = 10, 10, 10
+    lambda1, lambda2, lambda3 = 1.0, 1.0, 0.0
+    C1, C2 = 1.0, 1.0
+    u1, u2, u3 = 1.0, 1.0, 1.0
+
+
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
