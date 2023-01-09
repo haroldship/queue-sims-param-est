a5054d39f9be869847615427a5c07b4ef94d3875
Harold Ship <harold@il.ibm.com>
2023-01-09 16:46:02 +0200

    compute cost

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ def compute_x(x0, lam, mu, u, G, t):
     return x
 
 
+def compute_cost(c, x0, lam, mu, u, G, TT=10, dt=0.1):
+    J = 0
+    for t in np.arange(0.0, TT+dt, dt):
+        x_t = compute_x(x0, lam, mu, u, G, t)
+        J += c.dot(x_t)
+    return J
+
+
 if __name__ == '__main__':
     x01, x02, x03 = 10, 10, 10
     lam1, lam2, lam3 = lam = np.array((1.0, 1.0, 0.0))
     mu1, mu2, mu3 = mu = np.array((1.0, 1.0, 1.0))
-    C1 = 1.0
+    C1 = 1.0 # capacity of server 1
+    c = np.array((1.0, 1.0, 1.0))
     G = np.array(((1.0, 0, 0),(0, 1.0, 0),(0, -1.0, 1.0)))
-    T = 30
+    TT = 30
 
     np.random.seed(234)
 
@@ -36,5 +45,7 @@ if __name__ == '__main__':
             u3 = 1.0
             u = np.array((u1, u2, u3))
 
-            for t in range(0, T+1):
-                print(f'(u1,u2,u3) = ({(u1, u2, u3)}) x({t}) = {compute_x(x0, lam, mu, u, G, t)}')
+            print(compute_cost(c, x0, lam, mu, u, G, TT))
+
+            # for t in range(0, T+1):
+            #     print(f'(u1,u2,u3) = ({(u1, u2, u3)}) x({t}) = {compute_x(x0, lam, mu, u, G, t)}')
