f571809d1afa6d9fbdf156d9655a957face55834
Harold Ship <harold@il.ibm.com>
2023-01-19 18:20:40 +0200

    experiments work now

--- main.py
+++ main.py
@@ -8,37 +8,43 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy.random as npr
+import statistics
 
 
-def compute_x(x0, lam, mu, u, G, t):
-    x = np.maximum(x0 + lam * t - np.matmul(G, mu * u) * t, 0)
-    return x
+def compute_cost(queue_df, c, TT, sample_times):
+    cost = 0
+    K = len(c)
+    dt = TT / len(sample_times)
+    for t in sample_times:
+        x_t = np.array([q(queue_df, k+1, t) for k in range(K)])
+        cost += c.dot(x_t) * dt
+    return cost
 
 
-def compute_cost(c, x0, lam, mu, u, G, TT, dt=0.1):
-    J = 0
-    for t in np.arange(0.0, TT+dt, dt):
-        x_t = compute_x(x0, lam, mu, u, G, t)
-        J += c.dot(x_t)
-    return J
+def q(queue_df, k, t):
+    queue_df_k = queue_df[(queue_df.task_type == k)]
+    queue_df_kt = queue_df_k[(queue_df_k.time >= t)]
+    if queue_df_kt.size == 0:
+        if t < 1e-5:
+            return queue_df_k.iloc[0]['length']
+        else:
+            return queue_df_k.iloc[-1]['length']
+    return queue_df_kt.iloc[0]['length']
 
 
-def compute_variance(sigma_2, u, TT):
-    return sigma_2 * (1 / (u.dot(u) * TT))
-
-
-df = pd.DataFrame({'e': [], 'mc': [], 'J': [], 'sigma_2': []})
-
-
-task_df = pd.DataFrame(None, columns=('task_no', 'task_type', 'arrive_time', 'start_time', 'complete_time'))
-queue_df = pd.DataFrame(None, columns=('task_type', 'time', 'length'))
+def estimate_one_rate(queue_df, x0, lam, mu, u, G, sample_times, task_type):
+    task_index = task_type - 1
+    X_t = np.array([u[task_index] * t for t in sample_times]).reshape(len(sample_times), 1)
+    Y_t = np.array([-q(queue_df, task_type, t) + x0[task_index] + (lam[task_index] - sum([u[j] * G[task_index, j] * mu[j] for j in range(G.shape[1]) if G[task_index, j] < 0])) * t for t in sample_times]).reshape(len(sample_times), 1)
+    slope = np.linalg.inv(X_t.T.dot(X_t)).dot(X_t.T).dot(Y_t)
+    return slope[0,0]
 
 
 def process_task(env, service_time):
     yield env.timeout(service_time)
 
 
-def enter_network(env, G, buffers, task_type, mu, skip_queue=False):
+def enter_network(task_df, queue_df, env, G, buffers, task_type, mu, u, skip_queue=False):
     arrive_time = env.now
     task_index = task_type - 1
 
@@ -50,7 +56,7 @@ def enter_network(env, G, buffers, task_type, mu, skip_queue=False):
         start_time = env.now
         if not skip_queue:
             queue_df.loc[queue_df.size] = (task_type, start_time, len(queue.queue))
-        service_time = npr.exponential(1.0/mu[task_index])
+        service_time = npr.exponential(1.0/(u[task_index] * mu[task_index]))
         complete_time = start_time + service_time
         yield env.process(process_task(env, service_time))
         if not skip_queue:
@@ -62,10 +68,10 @@ def enter_network(env, G, buffers, task_type, mu, skip_queue=False):
             if j == task_index: continue
             p_kj = -G[j, task_index]
             if npr.binomial(1, p_kj):
-                env.process(enter_network(env, G, buffers, j+1, mu))
+                env.process(enter_network(task_df, queue_df, env, G, buffers, j+1, mu, u))
 
 
-def run_network(env, G, buffers, x0, lam, mu):
+def run_network(task_df, queue_df, env, G, buffers, x0, lam, mu, u):
     task_no = 0
     total_rate = np.sum(lam)
     probs = lam / total_rate
@@ -75,113 +81,67 @@ def run_network(env, G, buffers, x0, lam, mu):
             task_type = k + 1
             queue_df.loc[queue_df.size] = (task_type, 0, x0[k])
             task_no += 1
-            env.process(enter_network(env, G, buffers, task_type, mu, skip_queue=True))
+            env.process(enter_network(task_df, queue_df, env, G, buffers, task_type, mu, u, skip_queue=True))
 
     while True:
         yield env.timeout(npr.exponential(1.0 / total_rate))
         task_type = npr.choice(ntypes, p=probs) + 1
         task_no += 1
-        env.process(enter_network(env, G, buffers, task_type, mu))
-
-
-def q(k, t):
-    queue_df_k = queue_df[(queue_df.task_type == k)]
-    queue_df_kt = queue_df_k[(queue_df_k.time >= t)]
-    if queue_df_kt.size == 0:
-        if t < 1e-5:
-            return queue_df_k.iloc[0]['length']
-        else:
-            return queue_df_k.iloc[-1]['length']
-    return queue_df_kt.iloc[0]['length']
+        env.process(enter_network(task_df, queue_df, env, G, buffers, task_type, mu, u))
 
 
 def run_random_arrivals():
-    npr.seed(1)
 
-    x01, x02, x03 = x0 = np.array((10, 10, 10))
-    lam1, lam2, lam3 = lam = np.array((1.0, 1.0, 0.0))
-    mu1, mu2, mu3 = mu = np.array((1.0, 1.0, 1.0))
-    u1, u2, u3 = u = np.array((1.0, 1.0, 1.0))
-    C1, C2 = C = np.array((1.0, 1.0)) # capacity of servers 1, 2
+    x0 = np.array((50, 50, 50))
+    lam = np.array((2.0, 2.0, 0.0))
+    mu = np.array((2.0, 2.0, 1.0))
+    C = np.array((1.0, 1.0)) # capacity of servers 1, 2
     c = np.array((1.0, 1.0, 1.0)) # hold cost per item per unit time
     G = np.array(((1.0, 0, 0),(0, 1.0, 0),(0, -1.0, 1.0)))
-    TT = 30
-    dt = 0.5
 
-    env = simpy.Environment()
+    MC = 1000
 
-    x1, x2, x3, = buffers = [simpy.Resource(env, capacity=1) for i in range(3)]
+    stats_df = pd.DataFrame(None, columns=('T', 'N', 'u1', 'u2', 'u3', 'cost_mean', 'mu3_mean', 'mu3_var'), dtype='float')
 
-    env.process(run_network(env, G, buffers, x0, lam, mu))
-    env.run(TT)
-    task_df['wait_time'] = task_df.start_time - task_df.arrive_time
-    task_df['service_time'] = task_df.complete_time - task_df.start_time
-    task_df['sojourn_time'] = task_df.complete_time - task_df.arrive_time
+    for TT, n, u in [(30, 5, (1.0, 1.0, 1.0)),(30, 10, (1.0, 1.0, 1.0)),(30, 15, (1.0, 1.0, 1.0)), (30, 30, (1.0, 1.0, 1.0)), (30, 60, (1.0, 1.0, 1.0)), (30, 120, (1.0, 1.0, 1.0)), (30, 240, (1.0, 1.0, 1.0)), (30, 480, (1.0, 1.0, 1.0))]:
 
-    sample_times = np.arange(0, TT + dt, dt)
-    for t in sample_times:
-        print(f'q3({t})={q(3,t)}')
+        npr.seed(1)
 
-    X_t = np.array([u3 * t for t in sample_times]).reshape(len(sample_times), 1)
-    Y_t = np.array([-q(3,t) + x03 + (lam[2] + u2) * t for t in sample_times]).reshape(len(sample_times), 1)
+        costs = []
+        mu3_hats = []
 
-    mu3_hat = np.linalg.inv(X_t.T.dot(X_t)).dot(X_t.T).dot(Y_t)
-    print(mu3_hat)
+        for mc in range(2):
+            task_df = pd.DataFrame(None, columns=('task_no', 'task_type', 'arrive_time', 'start_time', 'complete_time'))
+            queue_df = pd.DataFrame(None, columns=('task_type', 'time', 'length'))
+            
+            dt = TT / n
+            u = np.array(u)
 
-    plt.scatter(X_t, Y_t)
-    plt.show()
+            env = simpy.Environment()
 
+            buffers = [simpy.Resource(env, capacity=1) for i in range(3)]
 
-def run_random_controls():
-    x01, x02, x03 = 10, 10, 10
-    lam1, lam2, lam3 = lam = np.array((1.0, 1.0, 0.0))
-    C1 = 1.0 # capacity of server 1
-    c = np.array((1.0, 1.0, 1.0))
-    G = np.array(((1.0, 0, 0),(0, 1.0, 0),(0, -1.0, 1.0)))
-    TT = 30
+            env.process(run_network(task_df, queue_df, env, G, buffers, x0, lam, mu, u))
+            env.run(TT)
+            task_df['wait_time'] = task_df.start_time - task_df.arrive_time
+            task_df['service_time'] = task_df.complete_time - task_df.start_time
+            task_df['sojourn_time'] = task_df.complete_time - task_df.arrive_time
 
-    np.random.seed(1234)
-    mu_mid = 1.0
-    mu_wid = 0.25
-    sigma_2 = mu_wid**2 / 12.0
-    Sigma_2 = np.eye(3) * sigma_2
+            sample_times = np.arange(0, TT + dt, dt)
+            mu3_hat = estimate_one_rate(queue_df, x0, lam, mu, u, G, sample_times, 3)
+            cost = compute_cost(queue_df, c, TT, sample_times)
 
-    MC = 100
-    u3s = []
+            mu3_hats.append(mu3_hat)
+            costs.append(cost)
 
-    for mc in range(MC):
-        for e in np.arange(0, 1.1, 0.1):
-            # Run the simulation in an environment
-            # env = simpy.Environment()
+        cost_mean = statistics.mean(costs)
+        mu3_hat_mean = statistics.mean(mu3_hats)
+        mu3_hat_var = statistics.variance(mu3_hats, mu3_hat_mean)
 
-            mu1, mu2, mu3 = mu = np.random.uniform(mu_mid - mu_wid / 2.0, mu_mid + mu_wid / 2.0, 3)
+        experiment_no = len(stats_df)
+        stats_df.loc[experiment_no] = (TT, n, *u, cost_mean, mu3_hat_mean, mu3_hat_var)
 
-            x0 = np.array((x01, x02, x03))
-
-            u1 = 1.0 - e
-            u2 = e
-            u3 = np.random.uniform(0.0, 1.0)
-            # if u1 + u2 > 1.0:
-            #     u1 = round(u1/(u1+u2), 2)
-            #     u2 = round(1.0 - u2, 2)
-            u = np.array((u1, u2, u3))
-
-            assert all(u >= 0), f"u={u}"
-            assert all(u <= 1.0), f"u={u}"
-
-            df.loc[len(df)] = [e, mc, compute_cost(c, x0, lam, mu, u, G, TT), compute_variance(Sigma_2, u, TT)[2,2]]
-            u3s.append(u3)
-
-    dfm = df.groupby('e')[['J', 'sigma_2']].mean()
-
-    plt.scatter(dfm.J, dfm.sigma_2)
-    plt.xlabel('J')
-    plt.title(r'Var($\hat{\beta}_3(u)$) vs $J(u)$')
-    plt.savefig('beta_hat-vs-J.pdf')
-    plt.cla()
-    plt.scatter(range(len(u3s)), u3s)
-    plt.title(r'$u_3$ in simulations')
-    plt.savefig('u3_values.pdf')
+    print(stats_df)
 
 
 if __name__ == '__main__':
